6c5d4928c72fb763ab05986599a7d5e610afb9ec
ferdynandpro <93565401+ferdynandpro@users.noreply.github.com>
2025-01-06 01:08:56 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Your backend URL
+  baseURL: "http://92.113.26.4:5000/api", // Your backend URL
   headers: {
     "Content-Type": "application/json",
   },
